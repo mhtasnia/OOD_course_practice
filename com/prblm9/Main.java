20c156098a659de7cafca0f820037c9583eacf73
Meherose Hossain Tasnia <mhtasnia@gmail.com>
2024-09-18 18:16:37 +0600

    problem9

---
+++ com/prblm9/Main.java
@@ -0,0 +1,18 @@
+package com.prblm9;
+public class Main {
+    public static void main(String[] args) {
+
+        Employee employee = new Employee();
+
+        employee.setEmployeeId(15);
+        employee.setEmployeeName("Caelius Dathan");
+        employee.setEmployeeSalary(4900.0);
+        int employeeId = employee.getEmployeeId();
+        String employeeName = employee.getEmployeeName();
+        String formattedSalary = employee.getFormattedSalary();
+		System.out.println("Employee Details:");
+        System.out.println("ID: " + employeeId);
+        System.out.println("Name: " + employeeName);
+        System.out.println("Salary: " + formattedSalary);
+    }
+}
